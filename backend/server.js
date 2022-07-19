165a4336c8f2fb0cf7b2c25df940426c3b1111be
anhphuonghihi <phuonghole121201@gmail.com>
2022-07-19 20:52:04 +0700

    server.js

---
+++ backend/server.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const app = express();
+require("dotenv").config();
+const port = process.env.PORT || 5000;
+const cors = require("cors");
+
+app.use(cors());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+const routes = require("./src/v1/routes");
+// app.use("/api", routes);
+app.listen(port, () =>
+  console.log("> Server is up and running on port : " + port)
+);
+
+const colors = require("colors");
+const mongoose = require("mongoose");
+mongoose
+  .connect(process.env.URI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then((res) => console.log("> Connected...".bgCyan))
+  .catch((err) =>
+    console.log(
+      `> Error while connecting to mongoDB : ${err.message}`.underline.red
+    )
+  );
