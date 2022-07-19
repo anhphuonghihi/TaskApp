71fb46dead8da07dbfe46f231e6d99155299d94f
anhphuonghihi <phuonghole121201@gmail.com>
2022-07-19 21:38:13 +0700

    boardSchema

---
+++ backend/models/board.js
@@ -0,0 +1,40 @@
+const mongoose = require("mongoose");
+const boardSchema = new mongoose.Schema(
+  {
+    user: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    icon: {
+      type: String,
+      default: "📃",
+    },
+    title: {
+      type: String,
+      default: "Untitled",
+    },
+    description: {
+      type: String,
+      default: `Add description here
+      🟢 You can add multiline description
+      🟢 Let's start...`,
+    },
+    position: {
+      type: Number,
+    },
+    favourite: {
+      type: Boolean,
+      default: false,
+    },
+    favouritePosition: {
+      type: Number,
+      default: 0,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+module.exports = mongoose.model("Board", boardSchema);
